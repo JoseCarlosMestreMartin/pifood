b7a2d504dd151f832929001c1a720acb84ac25e0
JoseCarlosMestreMartin <josecarlosmestremartin@gmail.com>
2023-07-20 09:44:11 -0300

    en pagination

--- client/src/Components/Home/home.jsx
+++ client/src/Components/Home/home.jsx
@@ -8,19 +8,27 @@ import { useDispatch, useSelector } from "react-redux";
 import { getRecipes } from "../../redux/actions";
 
 const Home = () => {
-    const dispatch = useDispatch();
-  
-    useEffect(() => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
     dispatch(getRecipes());
-    
   }, [dispatch]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [cantCardPerPage] = useState(9);
+  const indexOfLastCard = currentPage * cantCardPerPage;
+  const indexOfFirstCard = indexOfLastCard - cantCardPerPage;
+
+  const currentRecipes = allRecipes.slice(
+    indexOfFirstCard,
+    indexOfLastCard
+  );
 
   return (
     <div className={styles.container}>
       <NavBar />
       <h1>este es el Home</h1>
       <Cards />
-      <Pagination cantCardPerPage="8"  cantAllCards="200"  page="3" />
+      <Pagination cantCardPerPage="8" cantAllCards="200" page="3" />
     </div>
   );
 };
